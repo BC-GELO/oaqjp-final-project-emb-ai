ff5f229dd6d918473e1a1bc382dce0f0baffd02a
Gerardo <barranteslorenzo6@gmail.com>
2025-07-30 17:59:46 -0400

    This is an update

---
+++ emotion_detection.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json= { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = input_json, headers=header)
+    emotion_response = json.loads(response.text)
+    anger_score = emotion_response.get('anger', 0)
+    disgust_score = emotion_response.get('disgust', 0)
+    fear_score = emotion_response.get('fear', 0)
+    joy_score = emotion_response.get('joy', 0)
+    sadness_score = emotion_response.get('sadness', 0)
+    emotion_and_score = {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    }
+    dominant_emotion = max(emotion_and_score, key=emotion_and_score.get)
+    emotion_and_score['dominant_emotion'] = dominant_emotion
+    return emotion_and_score
